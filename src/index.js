6a95fc5b915abadb8a2fcd7596b0b21a99d838fe
Volodymyr-Bednov <109081369+Volodymyr-Bednov@users.noreply.github.com>
2023-03-19 22:27:13 +0200

    refactoring no localStorage

--- src/index.js
+++ src/index.js
@@ -2,18 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from 'components/App';
 import { Provider } from 'react-redux';
-import { store, persistor } from 'store';
-
-import { PersistGate } from 'redux-persist/integration/react';
+import { store } from 'store';
 
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
-      <PersistGate loading={null} persistor={persistor}>
-        <App />
-      </PersistGate>
+      <App />
     </Provider>
   </React.StrictMode>
 );
